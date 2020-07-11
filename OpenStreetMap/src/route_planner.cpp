ec8a9287feb5ca01b087d51bb18fee3c0adc5f52
Ethan Li <yanjia.li@snapchat.com>
2020-07-11 12:53:20 -0700

    finished TODO 6. construct final path and distance.

--- OpenStreetMap/src/route_planner.cpp
+++ OpenStreetMap/src/route_planner.cpp
@@ -79,6 +79,14 @@ std::vector<RouteModel::Node> RoutePlanner::ConstructFinalPath(RouteModel::Node 
     std::vector<RouteModel::Node> path_found;
 
     // TODO: Implement your solution here.
+    RouteModel::Node *next_node = current_node;
+    while (next_node->parent != nullptr)
+    {
+        distance += current_node->distance(*current_node->parent);
+        path_found.push_back(*current_node);
+        current_node = current_node->parent;
+    }
+    std::reverse(path_found.begin(), path_found.end());
 
     distance *= m_Model.MetricScale(); // Multiply the distance by the scale of the map to get meters.
     return path_found;

f84dc9f9ba3d8f9830331a67ad948a1213283610
Ethan Li <yanjia.li@snapchat.com>
2020-07-12 23:11:08 -0700

    parse system os, kernel and meminfo

--- SystemMonitor/src/linux_parser.cpp
+++ SystemMonitor/src/linux_parser.cpp
@@ -1,7 +1,10 @@
 #include <dirent.h>
+#include <sstream>
 #include <string>
 #include <unistd.h>
 #include <vector>
+#include <cassert>
+#include <iostream>
 
 #include "linux_parser.h"
 
@@ -11,19 +14,24 @@ using std::to_string;
 using std::vector;
 
 // DONE: An example of how to read data from the filesystem
-string LinuxParser::OperatingSystem() {
+string LinuxParser::OperatingSystem()
+{
   string line;
   string key;
   string value;
   std::ifstream filestream(kOSPath);
-  if (filestream.is_open()) {
-    while (std::getline(filestream, line)) {
+  if (filestream.is_open())
+  {
+    while (std::getline(filestream, line))
+    {
       std::replace(line.begin(), line.end(), ' ', '_');
       std::replace(line.begin(), line.end(), '=', ' ');
       std::replace(line.begin(), line.end(), '"', ' ');
       std::istringstream linestream(line);
-      while (linestream >> key >> value) {
-        if (key == "PRETTY_NAME") {
+      while (linestream >> key >> value)
+      {
+        if (key == "PRETTY_NAME")
+        {
           std::replace(value.begin(), value.end(), '_', ' ');
           return value;
         }
@@ -34,29 +42,35 @@ string LinuxParser::OperatingSystem() {
 }
 
 // DONE: An example of how to read data from the filesystem
-string LinuxParser::Kernel() {
-  string os, kernel;
+string LinuxParser::Kernel()
+{
+  string os, version, kernel;
   string line;
   std::ifstream stream(kProcDirectory + kVersionFilename);
-  if (stream.is_open()) {
+  if (stream.is_open())
+  {
     std::getline(stream, line);
     std::istringstream linestream(line);
-    linestream >> os >> kernel;
+    linestream >> os >> version >> kernel;
   }
   return kernel;
 }
 
 // BONUS: Update this to use std::filesystem
-vector<int> LinuxParser::Pids() {
+vector<int> LinuxParser::Pids()
+{
   vector<int> pids;
   DIR *directory = opendir(kProcDirectory.c_str());
   struct dirent *file;
-  while ((file = readdir(directory)) != nullptr) {
+  while ((file = readdir(directory)) != nullptr)
+  {
     // Is this a directory?
-    if (file->d_type == DT_DIR) {
+    if (file->d_type == DT_DIR)
+    {
       // Is every character of the name a digit?
       string filename(file->d_name);
-      if (std::all_of(filename.begin(), filename.end(), isdigit)) {
+      if (std::all_of(filename.begin(), filename.end(), isdigit))
+      {
         int pid = stoi(filename);
         pids.push_back(pid);
       }
@@ -67,7 +81,36 @@ vector<int> LinuxParser::Pids() {
 }
 
 // TODO: Read and return the system memory utilization
-float LinuxParser::MemoryUtilization() { return 0.0; }
+float LinuxParser::MemoryUtilization()
+{
+  string name, kilobytes;
+  string line;
+  std::ifstream stream(kProcDirectory + kMeminfoFilename);
+  if (stream.is_open())
+  {
+    int total_mem, free_mem;
+    for (int i = 0; i < 2; i++)
+    {
+      std::getline(stream, line);
+      std::istringstream linestream(line);
+      linestream >> name >> kilobytes;
+      switch (i)
+      {
+      case 0:
+        total_mem = stoi(kilobytes);
+        break;
+      case 1:
+        free_mem = stoi(kilobytes);
+        break;
+      default:
+        break;
+      }
+    }
+
+    return (total_mem - free_mem) / (float)total_mem;
+  }
+  return 0.0;
+}
 
 // TODO: Read and return the system uptime
 long LinuxParser::UpTime() { return 0; }

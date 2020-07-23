097b3e491fb4dc5924c9cd5ad7dccdd536310867
Ethan Li <yanjia.li@snapchat.com>
2020-07-23 16:36:55 -0700

    traffic: FP3

--- ConcurrentTrafficSimulation/src/TrafficLight.h
+++ ConcurrentTrafficSimulation/src/TrafficLight.h
@@ -9,6 +9,12 @@
 // forward declarations to avoid include cycle
 class Vehicle;
 
+enum class TrafficLightPhase
+{
+    red,
+    green
+};
+
 // FP.3 Define a class „MessageQueue“ which has the public methods send and receive.
 // Send should take an rvalue reference of type TrafficLightPhase whereas receive should return this type.
 // Also, the class should define an std::dequeue called _queue, which stores objects of type TrafficLightPhase.
@@ -18,7 +24,13 @@ template <class T>
 class MessageQueue
 {
 public:
+    void send(TrafficLightPhase &&);
+    TrafficLightPhase receive();
+
 private:
+    std::deque<TrafficLightPhase> _queue;
+    std::condition_variable _cond;
+    std::mutex _mtx;
 };
 
 // FP.1 : Define a class „TrafficLight“ which is a child class of TrafficObject.
@@ -26,11 +38,6 @@ private:
 // as well as „TrafficLightPhase getCurrentPhase()“, where TrafficLightPhase is an enum that
 // can be either „red“ or „green“. Also, add the private method „void cycleThroughPhases()“.
 // Furthermore, there shall be the private member _currentPhase which can take „red“ or „green“ as its value.
-enum class TrafficLightPhase
-{
-    red,
-    green
-};
 
 class TrafficLight : public TrafficObject
 {
